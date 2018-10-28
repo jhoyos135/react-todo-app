c5a495af1e3a1bbb4dfc8d6d566ffff4739aba62
Julian Hoyos <jhoyos135@gmail.com>
2018-10-28 10:38:31 -0400

    Basic todo app using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  state= {
+    todos: [
+      {id: 1, content: 'do homework'},
+      {id: 2, content: 'travel tomorrow'}
+    ]
+  }; //data 
+
+  deleteTodo = (id) => {
+
+    const todos = this.state.todos.filter(todo => {
+
+      return todo.id !== id
+
+    });
+    this.setState({
+      todos: todos
+    });
+
+  };
+  addTodo = (todo) => {
+
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos: todos
+    });
+
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+
+      <h1 className="center blue-text">Todo </h1>
+      <Todos deleteTodo= {this.deleteTodo} todos= {this.state.todos} />
+      <AddTodo addTodo= {this.addTodo} />
+
       </div>
     );
   }
